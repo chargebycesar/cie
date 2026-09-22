fdeef9f2a0a61227b839f7476fc37e101b3fe2d5
CESAR <instalacionesbufala@gmail.com>
2026-09-22 13:13:52 +0200

    Las pestanas de arriba bajan a dos lineas en el movil en vez de salirse

--- docs/motor.js
+++ docs/motor.js
@@ -5,9 +5,9 @@
  * Node para las pruebas.
  */
 
-import { t, coma, punto, mayus, sinAcentos, limpiarParaPdf } from "./util.js?v=202609221306";
-import { rellenarPdf } from "./relleno.js?v=202609221306";
-import { generarCie } from "./cie.js?v=202609221306";
+import { t, coma, punto, mayus, sinAcentos, limpiarParaPdf } from "./util.js?v=202609221313";
+import { rellenarPdf } from "./relleno.js?v=202609221313";
+import { generarCie } from "./cie.js?v=202609221313";
 
 export const MESES = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO",
   "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
