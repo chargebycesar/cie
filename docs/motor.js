96e06a374f5db0d3e65fa5aeac8ef433573d9761
CESAR <instalacionesbufala@gmail.com>
2026-09-22 14:09:38 +0200

    Sello de version

--- docs/motor.js
+++ docs/motor.js
@@ -5,9 +5,9 @@
  * Node para las pruebas.
  */
 
-import { t, coma, punto, mayus, sinAcentos, limpiarParaPdf } from "./util.js?v=202609221313";
-import { rellenarPdf } from "./relleno.js?v=202609221313";
-import { generarCie } from "./cie.js?v=202609221313";
+import { t, coma, punto, mayus, sinAcentos, limpiarParaPdf } from "./util.js?v=202609221407";
+import { rellenarPdf } from "./relleno.js?v=202609221407";
+import { generarCie } from "./cie.js?v=202609221407";
 
 export const MESES = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO",
   "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
